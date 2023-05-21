683cb723d4333d0fbf66883c1f13a6999f0a69f0
cnlamanna <cnlamanna@gmail.com>
2023-05-20 21:25:08 -0500

    final push for the PyBank, it is finished along with the export txt file, now gonna work on PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 budget_csv = os.path.join(".","resources","budget_data.csv")
 
+#identifying all variables used
 Total_Months = 0
 Total = 0
 Average_Change = 0
@@ -14,8 +15,15 @@ Greatest_Increase = 0
 Greatest_Increase_Month = 0
 #a variable that will help identify the largest increase
 Prior_Month_Value = 0
-profit_increase = 0
+# variables for largest decrease
+Greatest_Decrease = 0
+Greatest_Decrease_Month= 0
 
+# first print line and dots
+print("Financial Analysis")
+print("---------------------------------------")
+
+#calculated total montshs
 with open(budget_csv) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     next(csvreader)
@@ -33,7 +41,7 @@ with open(budget_csv) as csvfile:
         Total += profitlosses
 print("Total: ", Total)
 
-#calculate average change
+#calculate average change, you got this!!! Ask one of TA's to review to see if i'm doing too many extra steps
 with open(budget_csv) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     next(csvreader)
@@ -59,14 +67,47 @@ with open(budget_csv) as csvfile:
         date = row[0]
         Amount=int(row[1])
 
-        profit_increase = Amount - date
+        profit_increase = Amount - Previous_Value
 
         if profit_increase > Greatest_Increase:
             Greatest_Increase = profit_increase
             Greatest_Increase_Month = date
 
-            Prior_Month_Value = Amount
-print(f"Greatest Increase in Profits: {date} ({profit_increase})")
+        Previous_Value = Amount
+print(f"Greatest Increase in Profits: {Greatest_Increase_Month} ({Greatest_Increase})")
 
+#finding the greatest decrease in profits, including the date and $
+with open(budget_csv) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    next(csvreader)
+    for row in csvreader:
+        date = row[0]
+        Amount=int(row[1])
 
+        profit_decrease = Amount - Previous_Value
+
+        if profit_decrease < Greatest_Decrease:
+            Greatest_Decrease = profit_decrease
+            Greatest_Decrease_Month = date
+        
+        Previous_Value = Amount
+
+print(f"Greatest Decrease in Profits: {Greatest_Decrease_Month} ({Greatest_Decrease})")
+
+#trying to export the analysis 
+
+#identifying the txt file
+PyBank_Export_File = "PyBank_Analysis.txt"
+export_path = os.path.join(".", "analysis", PyBank_Export_File)
+
+with open(export_path, "w") as file:
+    file.write("Financial Analysis")
+    file.write("----------------------------")
+    file.write(f"Total Months: {Total_Months}")
+    file.write(f"Total: {Total}")
+    file.write(f"Average Change: {Average_Change_Dollars}")
+    file.write(f"Greatest Increase in Profits: {Greatest_Increase_Month} ({Greatest_Increase})")
+    file.write(f"Greatest Decrease in Profits: {Greatest_Decrease_Month} ({Greatest_Decrease})")
+        
+  #finished!!! the above code  export a text file to the analsysis folder in PyBank, if have extra time, try and break up the text to make it look nicer      
 
